f538349533e018cd389d4fcca0d647b89ea17bb4
Jacob Quintana <jacobquintaa@gmail.com>
2021-11-12 20:47:07 -0700

    Start buikd for game class

---
+++ game.js
@@ -0,0 +1,48 @@
+class Game {
+  constructor() {
+    this.user = new Player('Human', token);
+    this.computer  = new Player('Death', token)
+    this.classicGame = {
+    rock: {
+      img: 'rock-img',
+      beats: [ 'scicssors', 'magnet']
+    },
+      paper: {
+        img: 'paper-img',
+        beats: [ 'rock', 'magnet']
+      },
+      scissors: {
+        img: 'paper-img',
+        beats: [ 'PAPER', 'death']
+      }
+    }
+
+
+
+
+
+
+  }
+
+
+  computerChoice() {}
+    // a way to keep track of data from the game board
+    /* - create a way to track game pieces,
+       - what the pieces beat other pieces
+       - */
+
+    // a way to keep track of the selected game type
+    /* */
+
+    // a way to check the games board data for win conditions
+
+
+
+    // a way to detect when a game is a draw
+
+
+
+    //a way to reset the games board to begin a new game
+
+
+}
