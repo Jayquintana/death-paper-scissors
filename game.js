f03cae58df6bdccaf2aede641569741afea4620c
Jacob Quintana <jacobquintaa@gmail.com>
2021-11-14 19:43:20 -0700

    Make win contitions and hame pieces

--- game.js
+++ game.js
@@ -1,46 +1,63 @@
 class Game {
   constructor() {
-    this.user = new Player('Human', token);
-    this.computer  = new Player('Death', token)
-    this.classicGame = {
-    rock: {
-      img: 'rock-img',
-      beats: [ 'scicssors', 'magnet']
-    },
-      paper: {
-        img: 'paper-img',
-        beats: [ 'rock', 'magnet']
-      },
-      scissors: {
-        img: 'paper-img',
-        beats: [ 'PAPER', 'death']
-      }
-    }
-
-
-
-
-
-
+    this.user = new Player('Human', 'assets/exercise.png');
+    this.computer  = new Player('Death', 'assets/Death.svg')
+    this.winner;
+    this.classicPieces = ['rock','paper', 'scissors']
+    this.deathPieces = ['rock','paper', 'scissors', 'death', 'robots']
   }
 
+    checkForWin() {
+      if (this.user.weapon === 'rock' && this.computer.weapon === 'scissors' || this.computer.weapon === 'death') {
+        this.winner = this.user.name
+        this.user.updateWins()
+        return `${this.winner} Wins!`
+      } else if (this.user.weapon === 'rock' && this.computer.weapon === 'paper' || this.computer.weapon === 'robots') {
+        this.winner = this.computer.name
+        this.computer.updateWins()
+        return `${this.winner} Wins!`
+      }
 
-  computerChoice() {}
-    // a way to keep track of data from the game board
-    /* - create a way to track game pieces,
-       - what the pieces beat other pieces
-       - */
+      if (this.user.weapon === 'paper' && this.computer.weapon === 'rock' || this.computer.weapon === 'robots') {
+        this.winner = this.user.name
+        this.user.updateWins()
+        return `${this.winner} Wins!`
+      } else if (this.user.weapon === 'paper' && this.computer.weapon === 'scissors' || this.computer.weapon === 'death') {
+        this.winner = this.computer.name
+        this.computer.updateWins()
+        return `${this.winner} Wins!`
+      }
 
-    // a way to keep track of the selected game type
-    /* */
+      if (this.user.weapon === 'scissors' && this.computer.weapon === 'paper' || this.computer.weapon === 'death') {
+        this.winner = this.user.name
+        this.user.updateWins()
+        return `${this.winner} Wins!`
+      } else if (this.user.weapon === 'scissors' && this.computer.weapon === 'rock' || this.computer.weapon === 'robots') {
+        this.winner = this.computer.name
+        this.computer.updateWins()
+        return `${this.winner} Wins!`
+      }
 
-    // a way to check the games board data for win conditions
+      if (this.user.weapon === 'death' && this.computer.weapon === 'paper' || this.computer.weapon === 'robots') {
+        this.winner = this.user.name
+        this.user.updateWins()
+        return `${this.winner} Wins!`
+      } else if (this.user.weapon === 'death' && this.computer.weapon === 'rock' || this.computer.weapon === 'scissors') {
+        this.winner = this.computer.name
+        this.computer.updateWins()
+        return `${this.winner} Wins!`
+      }
 
-
-
-    // a way to detect when a game is a draw
-
-
+      if (this.user.weapon === 'robots' && this.computer.weapon === 'scissors' || this.computer.weapon === 'rock') {
+        this.winner = this.user.name
+        this.user.updateWins()
+        return `${this.winner} Wins!`
+      } else if (this.user.weapon === 'robots' && this.computer.weapon === 'death' || this.computer.weapon === 'paper') {
+        this.winner = this.computer.name
+        this.computer.updateWins()
+        return `${this.winner} Wins!`
+      }
+    }
 
     //a way to reset the games board to begin a new game
 
