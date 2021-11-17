1dc33f0d293c4246a0ceebe4b836ac8d40f1f572
Jacob Quintana <jacobquintaa@gmail.com>
2021-11-16 20:08:11 -0700

    Add death game functionality

--- main.js
+++ main.js
@@ -1,39 +1,32 @@
 //global variables
 var newGame = new Game();
 
-// dom related
-
 //sections
 var gameSelection = document.querySelector('.game-selection-box');
 var chooseFighterSection = document.querySelector('.choose-your-fighter');
 var chooseYourGameSection = document.querySelector('.choose-your-game-section');
-var pieces = document.querySelectorAll('.pieces')
-var gameImages = document.querySelector('.game-images')
-
+var pieces = document.querySelectorAll('.pieces');
+var allGamePieces = document.querySelector('.game-images');
+var classicGamePieces = document.querySelector('.rock-paper-scissors-img');
 //titles
 var chooseGameTitle = document.querySelector('.choose-game-title');
 var humanWinsTitle = document.querySelector('.human-wins');
 var deathTitle = document.querySelector('.death-wins');
-var drawTitle = document.querySelector('.its-a-draw')
-
-
+var drawTitle = document.querySelector('.its-a-draw');
 //imgs
-var deathAndRobotImg = document.querySelector('.death-robot-img')
-
+var deathAndRobotImg = document.querySelector('.death-robot-img');
 //buttons
 var classicGameButton = document.querySelector('.classic-game');
 var deathGameButton = document.querySelector('.death-game');
-var changeGameButton = document.querySelector('.change-game-box')
+var changeGameButton = document.querySelector('.change-game-box');
 
-//event listeners
-gameImages.addEventListener('click',playClassicGame )
+allGamePieces.addEventListener('click', playDeathGame)
+classicGamePieces.addEventListener('click', playClassicGame)
 chooseYourGameSection.addEventListener('click', chooseGame);
-classicGameButton.addEventListener('click',goToClassicGame);
-deathGameButton.addEventListener('click',goToDeathGame);
+classicGameButton.addEventListener('click', goToClassicGame);
+deathGameButton.addEventListener('click', goToDeathGame);
 changeGameButton.addEventListener('click', changeGame)
 
-
-//reusable functions
 function hideElement(element) {
   element.classList.add('hidden');
 }
@@ -42,75 +35,87 @@ function showElement(element) {
   element.classList.remove('hidden');
 }
 
-//hide button and show game
 function goToClassicGame() {
   hideElement(gameSelection);
   hideElement(deathAndRobotImg);
-  showElement(chooseFighterSection)
-  hideElement(changeGameButton)
+  showElement(chooseFighterSection);
+  hideElement(changeGameButton);
   chooseGameTitle.innerText = 'Choose Your Fighter!'
 }
 
 function goToDeathGame() {
-  hideElement(gameSelection)
-  showElement(chooseFighterSection)
+  hideElement(gameSelection);
+  showElement(chooseFighterSection);
   chooseGameTitle.innerText = 'Choose Your Fighter!'
 }
 
-//game selection
 function chooseGame() {
   if (event.target.id === 'classic') {
-    goToClassicGame()
-    showElement(changeGameButton)
+    goToClassicGame();
+    showElement(changeGameButton);
+    newGame.chooseGameStyle('classic');
   }  else if (event.target.id === 'death') {
-    goToDeathGame()
-    showElement(changeGameButton)
+    goToDeathGame();
+    showElement(changeGameButton);
+    newGame.chooseGameStyle('death');
   }
 }
 
 function changeGame() {
   showElement(gameSelection);
   showElement(deathAndRobotImg);
-  hideElement(chooseFighterSection)
+  hideElement(chooseFighterSection);
   chooseGameTitle.innerText = 'Choose Your Game!'
 }
 
-
-//choose fighter
 function playClassicGame() {
+  if (newGame.style === 'classic') {
   newGame.startClassicGame(event.target.id)
-  newGame.checkForWin()
-  addWins()
-  console.log(newGame);
+  newGame.checkForWin();
+  addWins();
   chooseGameTitle.innerText = `${newGame.winner}`
   showGamePieces(newGame.user.weapon, newGame.computer.weapon);
-  setTimeout(resetGame, 3000)
+  setTimeout(resetGame, 2000);
+  }
+}
+
+function playDeathGame() {
+  if (newGame.style === 'death') {
+    newGame.startDeathGame(event.target.id)
+    newGame.checkForWin();
+    addWins();
+    console.log(newGame.winner);
+    chooseGameTitle.innerText = `${newGame.winner}`
+    showGamePieces(newGame.user.weapon, newGame.computer.weapon);
+    setTimeout(resetGame, 2000);
+  }
 }
 
 function addWins() {
   if (newGame.winner === 'Human Wins!') {
-    newGame.user.updateWins()
-    humanWinsTitle.innerText = `Wins: ${newGame.user.wins}`
+    newGame.user.updateWins();
+    humanWinsTitle.innerText = `Wins: ${newGame.user.wins}`;
   } else if (newGame.winner === 'Death Wins!') {
-      newGame.computer.updateWins()
-      deathTitle.innerText = `Wins: ${newGame.computer.wins}`
+      newGame.computer.updateWins();
+      deathTitle.innerText = `Wins: ${newGame.computer.wins}`;
   }
 }
 
 function resetGame() {
-  chooseGameTitle.innerText = 'Choose Your Fighter!'
-  hideGamePieces()
+  chooseGameTitle.innerText = 'Choose Your Fighter!';
+  hideGamePieces();
   hideElement(drawTitle);
-
+  allGamePieces.classList.remove('pointer-none');
 }
 
 function showGamePieces(humanWeapon, computerWeapon) {
   for (var i = 0; i < pieces.length; i++) {
     hideElement(pieces[i]);
+    allGamePieces.classList.add('pointer-none');
     if (humanWeapon === pieces[i].id || computerWeapon === pieces[i].id) {
       showElement(pieces[i]);
     } else if (humanWeapon === computerWeapon) {
-      showElement(drawTitle)
+      showElement(drawTitle);
     }
   }
 }
@@ -120,23 +125,3 @@ function hideGamePieces() {
     showElement(pieces[i]);
   }
 }
-
-function displayUserIcon() {
-var displayIcon = document.querySelectorAll('.display-choice')
-showElement(element)
-setTimeout(hideElement(element), 5000)
-// need to display the player icon with accosiated piece selected
-// need a way to display the user and the computers choice to compare them and only for a split second
-//
-}
-
-
-
-
-// need to do
-// toggle pieces within a win or a Draw
-//update game wins when a game is played
-// set timeout for the game after a game is played
-//incorperate reset game
-//find a way to choose game selection
-//create a button that allows the user to go choose another game
